df62c7cd974fc6152bc525e1fbe5f97db60f3297
gixgine-budi <miqbalr001@gmail.com>
2024-11-05 06:05:58 +0700

    new edits

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(){
     printf("Hello world!\n");
     printf("greetings!\n");
     printf("I didn't expect that...\n");
-    printf("How magical...");
+    printf("How magical...\n");
+    printf("How about this?");
 }
